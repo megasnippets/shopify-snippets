6d55fa2b75383f55f09301d444aff7e3623502f4
owenrockwell <32755202+owenrockwell@users.noreply.github.com>
2021-04-15 12:45:31 +1000

    Create theme.js

---
+++ hover-position-zoom/theme.js
@@ -0,0 +1,6 @@
+const el = document.querySelector("#hoverzoom");
+
+el.addEventListener("mousemove", (e) => {
+  el.style.setProperty('--x', -e.offsetX + "px");
+  el.style.setProperty('--y', -e.offsetY + "px");
+});
