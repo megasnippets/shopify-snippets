2e2ecc0bbb22efe5bb14f9c0eb51ff21425858cd
owenrockwell <32755202+owenrockwell@users.noreply.github.com>
2021-07-20 16:35:49 +1000

    Update theme.js
    Added click-to-zoom, for cases where there is no click action.

--- hover-position-zoom/theme.js
+++ hover-position-zoom/theme.js
@@ -1,5 +1,14 @@
 const el = document.querySelector("#hoverzoom");
 
+/* for click-to-zoom // add class .zooming to CSS
+hoverZoom.addEventListener("click", (e) => {
+  hoverZoom.style.setProperty('--x', parseInt(-e.offsetX / 2) + "px");
+  hoverZoom.style.setProperty('--y', parseInt(-e.offsetY / 2) + "px");
+  $("#hoverzoom .slick-slide.slick-current.slick-active").toggleClass("zooming");
+  
+});
+*/
+
 el.addEventListener("mousemove", (e) => {
   el.style.setProperty('--x', -e.offsetX + "px");
   el.style.setProperty('--y', -e.offsetY + "px");
